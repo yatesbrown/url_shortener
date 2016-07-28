dc6ded638294647b98e101c5167a8678fabd0589
Yates Brown <ybyates92@gmail.com>
2016-07-28 18:45:48 -0400

    config database update

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160728200911) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "url"
     t.datetime "created_at", null: false
